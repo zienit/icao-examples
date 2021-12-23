caf08536c001bb29999a0bfdeeff4023575f1c80
Henri Bezemer <7794279-zienit@users.noreply.gitlab.com>
2021-12-23 16:42:00 +0100

    DG14

--- src/test/java/nl/zienit/icao_examples/PATest.java
+++ src/test/java/nl/zienit/icao_examples/PATest.java
@@ -1,12 +1,15 @@
 package nl.zienit.icao_examples;
 
 import net.sf.scuba.tlv.TLVInputStream;
-import org.bouncycastle.asn1.ASN1OctetString;
+import org.bouncycastle.asn1.*;
+import org.bouncycastle.asn1.bsi.BSIObjectIdentifiers;
 import org.bouncycastle.asn1.cms.CMSObjectIdentifiers;
 import org.bouncycastle.asn1.cms.ContentInfo;
 import org.bouncycastle.asn1.cms.SignedData;
 import org.bouncycastle.asn1.icao.ICAOObjectIdentifiers;
 import org.bouncycastle.asn1.icao.LDSSecurityObject;
+import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
+import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
 import org.bouncycastle.cert.X509CertificateHolder;
 import org.bouncycastle.cms.CMSSignedData;
 import org.bouncycastle.cms.jcajce.JcaSimpleSignerInfoVerifierBuilder;
@@ -17,6 +20,7 @@ import org.junit.Test;
 import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.util.Formatter;
+import java.util.Set;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.greaterThan;
@@ -27,6 +31,21 @@ import static org.junit.Assert.assertThat;
 // lead to an exception being thrown when parsing Ef.COM. For this reason, two different
 // libraries are used. See also this discussion: https://github.com/bcgit/bc-java/issues/1081
 public class PATest {
+
+    // ICAO 9303 part 11, section 9.2.3 PACE Object Identifier
+    static final ASN1ObjectIdentifier id_PACE = BSIObjectIdentifiers.bsi_de.branch("2.2.4");
+    static final ASN1ObjectIdentifier id_PACE_DH_GM = id_PACE.branch("1");
+    static final ASN1ObjectIdentifier id_PACE_DH_IM = id_PACE.branch("3");
+    static final ASN1ObjectIdentifier id_PACE_ECDH_GM = id_PACE.branch("2");
+    static final ASN1ObjectIdentifier id_PACE_ECDH_IM = id_PACE.branch("4");
+    static final ASN1ObjectIdentifier id_PACE_ECDH_CAM = id_PACE.branch("6");
+
+    // ICAO 9303 part 11, section 9.2.7 Chip Authentication Object Identifier
+    static final ASN1ObjectIdentifier id_CA = BSIObjectIdentifiers.bsi_de.branch("2.2.3");
+    static final ASN1ObjectIdentifier id_PK = BSIObjectIdentifiers.bsi_de.branch("2.2.1");
+
+    // ICAO 9303 part 11, section 9.2.9 Terminal Authentication Object Identifiers
+    static final ASN1ObjectIdentifier id_TA = BSIObjectIdentifiers.bsi_de.branch("2.2.2");
 
     @Test
     // ICAO 9303 Part 10, section 4.6.2 Document Security Object EF.SOD
@@ -63,8 +82,8 @@ public class PATest {
             final var isSignerInfoVerified = signerInfo.verify(new JcaSimpleSignerInfoVerifierBuilder().build(documentSignerCert));
             assertThat(isSignerInfoVerified, equalTo(true));
 
-            // note: validation of the Document Signer certificate itself is skipped in this test,
-            // but MUST be done in production applications.
+            // note: validation of the Document Signer certificate itself against CSCA cert and CRL is skipped
+            // in this test, but MUST be done in production applications!
 
             final var eci = sd.getEncapContentInfo();
 
@@ -151,6 +170,87 @@ public class PATest {
                     System.out.println(new String(elements.readValue()));
                 }
             }
+        }
+    }
+
+    // ICAO 9303 Part 10, section 4.7.14 DATA GROUP 14 — Security Options
+    @Test
+    public void testReadDG14() throws Exception {
+
+        // pre-condition: read from EF.SOD
+        final var digestAlgorithmIdentifier = "2.16.840.1.101.3.4.2.1";
+        final var hashValue = Hex.decode("a7d6f535b1c2b2f466f167d3092e39e5b8b557a4beea463500844063d2d08497");
+
+        try (final var fis = new FileInputStream(System.getProperty("user.home") + "/Documents/passive_auth/dg14")) {
+
+            // hash must be equal to hash listed in EF.SOD
+            final var contents = fis.readAllBytes();
+            final var digestAlgorithm = DigestFactory.getDigest(digestAlgorithmIdentifier);
+            digestAlgorithm.update(contents, 0, contents.length);
+            final var digest = new byte[digestAlgorithm.getDigestSize()];
+            digestAlgorithm.doFinal(digest, 0);
+            assertThat(digest, equalTo(hashValue));
+
+            try (final var dg14 = new TLVInputStream(new ByteArrayInputStream(contents))) {
+
+                assertThat(dg14.readTag(), equalTo(0x6E));
+                dg14.readLength();
+                final var securityInfos = ASN1Set.getInstance(dg14.readValue());
+
+                final var out = new Formatter(System.out);
+
+                for (final var securityInfo : securityInfos.toArray()) {
+
+                    final var i = ((ASN1Sequence) securityInfo).iterator();
+                    final var protocol = (ASN1ObjectIdentifier) i.next();
+
+                    if (protocol.on(id_PACE)) {
+
+                        if (Set.of(id_PACE_DH_GM, id_PACE_DH_IM, id_PACE_ECDH_GM, id_PACE_ECDH_IM, id_PACE_ECDH_CAM).contains(protocol)) {
+                            // ICAO 9303 Part 11, section 9.2.2 PACEDomainParameterInfo
+                            final var domainParameter = AlgorithmIdentifier.getInstance(i.next());
+                            final var parameterId = i.hasNext() ? ((ASN1Integer) i.next()).getValue() : null;
+                            out.format("PACEDomainParameterInfo %s %d", domainParameter.getAlgorithm(), parameterId);
+
+                        } else {
+                            // ICAO 9303 Part 11, section 9.2.1 PACEInfo
+                            final var version = ((ASN1Integer) i.next()).getValue();
+                            final var parameterId = (i.hasNext()) ? ((ASN1Integer) i.next()).getValue() : null;
+                            out.format("PACEInfo %s %d %d\n", protocol, version, parameterId);
+                        }
+
+                    } else if (protocol.equals(ICAOObjectIdentifiers.id_icao_aaProtocolObject)) {
+                        // ICAO 9303 Part 11, section 9.2.4 ActiveAuthenticationInfo
+                        final var version = ((ASN1Integer) i.next()).getValue();
+                        final var signatureAlgorithm = (ASN1ObjectIdentifier) i.next();
+                        out.format("ActiveAuthenticationInfo %d %s\n", version, signatureAlgorithm);
+
+                    } else if (protocol.on(id_CA)) {
+                        // ICAO 9303 Part 11, section 9.2.5 ChipAuthenticationInfo
+                        final var version = ((ASN1Integer) i.next()).getValue();
+                        final var keyId = (i.hasNext()) ? ((ASN1Integer) i.next()).getValue() : null;
+                        out.format("ChipAuthenticationInfo %d %d\n", version, keyId);
+
+                    } else if (protocol.on(id_PK)) {
+                        // ICAO 9303 Part 11, section 9.2.6 ChipAuthenticationPublicKeyInfo
+                        final var chipAuthenticationPublicKey = SubjectPublicKeyInfo.getInstance(i.next());
+                        final var keyId = (i.hasNext()) ? ((ASN1Integer) i.next()).getValue() : null;
+                        out.format("ChipAuthenticationPublicKeyInfo %s %.40s... %d\n",
+                                chipAuthenticationPublicKey.getAlgorithm().getAlgorithm(),
+                                chipAuthenticationPublicKey.getPublicKeyData(),
+                                keyId);
+
+                    } else if (protocol.equals(id_TA)) {
+                        // ICAO 9303 part 11, section 9.2.8 TerminalAuthenticationInfo
+                        final var version = ((ASN1Integer) i.next()).getValue();
+                        out.format("TerminalAuthenticationInfo %d\n", version);
+                    }
+                    // ICAO 9303 part 11, section 9.2: SecurityInfos MAY contain additional entries indicating support for
+                    // other protocols or providing other information. The inspection system MAY discard any unknown entry.
+                }
+            }
+
+
         }
     }
 }
